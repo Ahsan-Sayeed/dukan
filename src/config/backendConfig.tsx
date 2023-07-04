8c09f779384c4a787b86d3067cdf5d7f732b4f11
Ahsan-Sayeed <ahsansayeedsaad@gmail.com>
2023-07-04 20:07:15 +0600

    vercel issu 1

--- src/config/backendConfig.tsx
+++ src/config/backendConfig.tsx
@@ -1,4 +1,5 @@
-export const url = 'https://dukan-server-daiu7oxok-ahsan-sayeed.vercel.app/';
+// export const url = 'https://dukan-server-daiu7oxok-ahsan-sayeed.vercel.app/';
 // export const url = 'http://localhost:4000/';
+export const url = 'https://dukan-server.vercel.app/';
 
 
